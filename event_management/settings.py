e47a5538b338da1d6881e277b2d7d83e503aafc5
snayeemu <snayeemu2@gmail.com>
2025-02-19 20:07:35 +0600

    host added

--- event_management/settings.py
+++ event_management/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-)rh$zh1udw$=d@w9kaj6qphwds#pk=5(in&r3#d$w5z56yx9#o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGIN = ["https://*.onrender.com", "http://127.0.0.1:8000"]
 
 
 # Application definition
