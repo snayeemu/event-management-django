e5c7eee6e97bc3564510496802d6bcec7d6d6c35
snayeemu <snayeemu2@gmail.com>
2025-03-07 04:10:19 +0600

    models registered for admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from events import models
 
 # Register your models here.
+admin.site.register(models.Category)
+admin.site.register(models.Event)
+admin.site.register(models.Participant)
